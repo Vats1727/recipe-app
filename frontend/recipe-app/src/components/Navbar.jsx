cbd3c13379e7b4f61fed4c1e987fc5bffac9ba49
Vats1727 <vatsalparmar1742002@gmail.com>
2025-02-23 09:58:03 +0530

    Some changes in style of nav bar

--- frontend/recipe-app/src/components/Navbar.jsx
+++ frontend/recipe-app/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-
 import { Link } from "react-router-dom";
+
 
 const Navbar = () => (
   <nav className="navbar">
-    <h1>Recipe App</h1>
-    <div>
+    <h1 className="logo">Recipe App</h1>
+    <div className="nav-links">
       <Link to="/">Home</Link>
       <Link to="/signup">Signup</Link>
       <Link to="/login">Login</Link>
       <Link to="/form">User Form</Link>
+    </div>
+    <div className="search-container">
+      <input type="text" placeholder="Search for recipes..." />
+      <button>Search</button>
     </div>
   </nav>
 );
